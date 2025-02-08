30656f4b1e4645617a6f470b1b4d848e082eb4db
Micilini <sdanz@hotmail.com>
2025-02-08 20:31:35 -0300

    Lição 3

---
+++ 2-convencoes-em-go/handlers.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func HandleRequest() {
+    fmt.Println("Estes são os handlers...")
+}
+
+func main() {
+    fmt.Println("Olá, mundo") 
+}
---
+++ 2-convencoes-em-go/utils.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func SayUtils() {
+    fmt.Printf("Olá utils...")
+}
+
+func main() {
+    fmt.Println("Olá, mundo") 
+}
